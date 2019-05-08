320eb9cfcca994d17e6056df4b40d929faff1743
赖金川 <lai_jc@126.com>
2019-05-08 20:29:47 +0800

    restFul协议

--- src/main/java/com/ljc/controller/jsonTest.java
+++ src/main/java/com/ljc/controller/jsonTest.java
@@ -2,6 +2,7 @@ package com.ljc.controller;
 
 import com.ljc.po.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,11 +11,23 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class jsonTest {
     @RequestMapping("/requestJson")
-    public @ResponseBody User requestJson(@RequestBody User user){
+    public @ResponseBody
+    User requestJson(@RequestBody User user) {
         return user;
     }
+
     @RequestMapping("/responseJson")
-    public @ResponseBody User responseJson( User user){
+    public @ResponseBody
+    User responseJson(User user) {
         return user;
     }
+
+    @RequestMapping("restFul/{id}")
+    public @ResponseBody User restQuery(@PathVariable("id") Integer id) {
+        User user = new User();
+        user.setOrder(1);
+        user.setUsername("赖金川");
+        user.setPassword("LJC");
+        return user;}
 }
+
